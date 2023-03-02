d6116c72c43aa4e155dcd13e0f12b4d3033ad76a
Meteor_Z <3030218083@qq.com>
2023-03-02 15:38:18 +0800

    优化

--- config.h
+++ config.h
@@ -5,7 +5,9 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+// static const char *fonts[]          = { "monospace:size=10" };
+// 修改了字体
+static const char *fonts[]          = {"JetBrainsMono Nerd Font:size=12","JetBrainsMono Nerd Font:pixelsize=14:type=2048-em:antialias=true::qahint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -57,7 +59,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+
+// 加入控制栏
+static const char *dmenucmd[] = {"rofi", "-show", "drun", NULL};
+
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
